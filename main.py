8b1e18a96b3d9bb57f63a3b835192e4b2cffc04d
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-02-21 22:14:55 +0200

    update

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ new_names = [name.upper() for name in names if name != name.upper()]
 print(new_names)
 
 with open('work_log.txt', 'a') as file:
-    file.writelines('\n19.02')
+    file.writelines('\n21.02')
