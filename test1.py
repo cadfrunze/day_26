4b15eb83681defbf6e70f33582d534531a348659
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-02-21 22:53:33 +0200

    update

--- test1.py
+++ test1.py
@@ -1,12 +1,13 @@
 import pandas
 data_brut = {
     "useri": ['Maryus', 'Bianca', 'Iulia', 'Sebi'],
-    "scor:": [1, 2, 3, 4]
+    "scor": [1, 2, 3, 4]
 }
 
 data_pd = pandas.DataFrame(data_brut)
 
-
-for (cheia, valoarea) in data_pd.iterrows():
-    print(valoarea)
+# print(data_pd)
+for (index, valoarea) in data_pd.iterrows():
+    if valoarea.useri == "Maryus":
+        print(valoarea.scor)
 
