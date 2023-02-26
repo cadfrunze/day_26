47e7e1efb9303776a30221d95742d6054ea97d5d
cadfrunze <marius1890@yahoo.com>
2023-02-26 18:46:55 +0200

    update

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-def formula(a, b, c):
-    return (a + b) ** c
+import pandas
+
+# def formula(a, b, c):
+#     return (a + b) ** c
+
 
 #
 # ecuatia = formula(a=2, b=3, c=2)
@@ -17,8 +20,26 @@ weather_c = {
     "Sunday": 24,
 }
 
-list_test = [item for item in weather_c.keys()]
-print(list_test)
+# list_test = [item for item in weather_c.keys()]
+# print(list_test)
+
+# for (key, value) in weather_c.items():
+#     print(key, value)
+
+
+data_dict = {
+    'useri': ['maryus', 'bianca', 'test_useri'],
+    'scor': [1, 2, 'test_scor']
+}
+
+data_test = pandas.DataFrame(data_dict)
+#
+# for (cheia, valoare) in data_test.items():
+#     print(valoare)
+
+for (index, row.useri) in data_test.iterrows():
+    print(row.useri)
+
 
 
 
